6d27828875022cc7bbfc7d3fb9df9e8c553f63a3
とちざわ <stochiz@jops.co.jp>
2021-11-05 19:04:40 +0900

    Update README.md

--- src/main/java/com/example/project/Hello.java
+++ src/main/java/com/example/project/Hello.java
@@ -1,10 +1,8 @@
 package com.example.project;
 
 public class Hello {
-
-	public static void main(final String[] args) {
-		System.out.println("Not the right string, tests will fail!");
-	}
-
+  public static void main(final String[] args) {
+    System.out.println("Hello world!");
+  }
 }
 
--- src/test/java/com/example/project/TestHello.java
+++ src/test/java/com/example/project/TestHello.java
@@ -2,29 +2,25 @@ package com.example.project;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
 
 import java.io.*;
 
 public class TestHello {
 
-   @Test
-   public void testHelloWorld()
-   {
-     PrintStream originalOut = System.out;
-     ByteArrayOutputStream bos = new ByteArrayOutputStream();
-     System.setOut(new PrintStream(bos));
+  @Test
+  public void testHelloWorld() {
+    PrintStream originalOut = System.out;
+    ByteArrayOutputStream bos = new ByteArrayOutputStream();
+    System.setOut(new PrintStream(bos));
 
-     // action
-     Hello.main(null);
+    // action
+    Hello.main(null);
 
-     // assertion
-     assertEquals("Hello world!\n", bos.toString());
+    // assertion
+    assertEquals("Hello world!\n", bos.toString());
 
-     // undo the binding in System
-     System.setOut(originalOut);
-   }
+    // undo the binding in System
+    System.setOut(originalOut);
+  }
 }
